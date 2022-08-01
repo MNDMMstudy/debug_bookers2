800afeed46578c2a93083d33e7fcac1c97a6f96b
MNDMMstudy <107236809+MNDMMstudy@users.noreply.github.com>
2022-08-01 09:09:23 +0000

    [finish]step_up_exercise6

--- Rails6-bookers2-debug-main/app/controllers/post_comments_controller.rb
+++ Rails6-bookers2-debug-main/app/controllers/post_comments_controller.rb
@@ -1,16 +1,16 @@
 class PostCommentsController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
-    comment =current_user.post_comments.new(post_comment_params)
-    comment.book_id = book.id
-    comment.save
-    redirect_to book_path(book)
+    @books = Book.find(params[:book_id])
+    @post_comment =current_user.post_comments.new(post_comment_params)
+    @post_comment.book_id = @books.id
+    @post_comment.save
   end
 
   def destroy
-    @post_comment = PostComment.find(params[:id])
+    @books = Book.find(params[:book_id])
+    @post_comment = PostComment.find_by(id: params[:id], book_id: params[:book_id])
     @post_comment.destroy
-    redirect_to books_path
+    @post_comment = PostComment.new
   end
 
   private
